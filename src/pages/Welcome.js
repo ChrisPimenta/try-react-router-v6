c0aa05cebf71d7a9f02ebf25baf0c5f15af48b00
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-25 12:29:47 +0100

    Outlet to define where nested element goes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Welcome = () => {
   // Router v6 uses relative paths so we do not need to repeat the full path of welcome/new-user. Based on the parent route, it already knows to include it
@@ -6,6 +6,7 @@ const Welcome = () => {
     <section>
       <h1>The Welcome Page</h1>
       <Link to="new-user">New User</Link>
+      <Outlet />
     </section>
   )
 };
