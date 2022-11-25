6ce15c7bb465cacb724126ead8ce6ea8c4eb579c
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-25 12:28:08 +0100

    Move nested routes up to app

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   // Router v6 uses relative paths so we do not need to repeat the full path of welcome/new-user. Based on the parent route, it already knows to include it
@@ -6,9 +6,6 @@ const Welcome = () => {
     <section>
       <h1>The Welcome Page</h1>
       <Link to="new-user">New User</Link>
-      <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
     </section>
   )
 };
