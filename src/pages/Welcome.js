0e79130142dd0d644a7ec902313162ce2c04f951
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-25 12:25:09 +0100

    Welcome new user link with relative path

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 const Welcome = () => {
   // Router v6 uses relative paths so we do not need to repeat the full path of welcome/new-user. Based on the parent route, it already knows to include it
   return (
     <section>
       <h1>The Welcome Page</h1>
+      <Link path="new-user">New User</Link>
       <Routes>
         <Route path="new-user" element={<p>Welcome, new user!</p>} />
       </Routes>
