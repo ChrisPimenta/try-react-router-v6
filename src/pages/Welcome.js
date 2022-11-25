019a4531485ba25ec2714a10d75e3bebd593cdf4
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-25 12:23:06 +0100

    Nested routing and redirects

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  // Router v6 uses relative paths so we do not need to repeat the full path of welcome/new-user. Based on the parent route, it already knows to include it
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes>
+    </section>
+  )
 };
 
 export default Welcome;
